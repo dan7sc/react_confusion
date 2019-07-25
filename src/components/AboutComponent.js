84a3523375da66142553e63d21a447f6f34022f8
daniel sc <su7cheng@gmail.com>
2019-07-25 15:02:34 -0300

    Fetch from Server.

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -24,7 +24,7 @@ function About(props) {
 
     const leaders = props.leaders.map((leader) => {
         return (
-            <div>
+            <div key={leader.id}>
                 <RenderLeader leader={leader} />
             </div>
         );
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = (val) => val && val.length;
@@ -100,7 +101,7 @@ function RenderDish({dish}) {
     return (
         <div key={dish.id} className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
+                <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
