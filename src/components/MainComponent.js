5054c468508cd360717c0e6d23f5ece2afe830f5
daniel sc <su7cheng@gmail.com>
2019-07-25 16:23:32 -0300

    React Animation Components.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -81,15 +81,6 @@ class Main extends Component {
             </Switch>
           </CSSTransition>
         </TransitionGroup>
-
-        {/* <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes} />} />
-          <Route path="/menu/:dishId" component={DishWithId} />
-          <Route exact path="/aboutus" component={() => <About leaders={this.props.leaders} />} />
-    <Route exact path="/contactus" component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-          <Redirect to="/home" />
-        </Switch> */}
         <Footer />
       </div>
     );
