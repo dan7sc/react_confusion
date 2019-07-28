5f97bdd2b4a96a5d1c3b0ecbb793c65c8ce69259
daniel sc <su7cheng@gmail.com>
2019-07-28 16:22:42 -0300

    Assignment 4.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -94,7 +94,7 @@ class CommentForm extends Component {
                     </ModalBody>
                 </Modal>
             </div>
-        )
+        );
     }
 }
 
@@ -118,83 +118,83 @@ function RenderDish({dish}) {
 }
 
 function RenderComments({comments, postComment, dishId}) {
-        if (comments != null) {
-            return (
-                <div className="col-12 col-md-5 m-1">
-                    <h4>Comments</h4>
-                    <ul className="list-unstyled">
-                        <Stagger in>
-                            {comments.map((comment) => {
-                                return(
-                                    <Fade in>
-                                        <li key={comment.id}>
-                                            <p>{comment.comment}</p>
-                                            <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                                        </li>
-                                    </Fade>
-                                );
-                            })}
-                        </Stagger>
-                    </ul>
-                    <CommentForm dishId={dishId} postComment={postComment} />
-                </div>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            );
-        }
+    if (comments != null) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    <Stagger in>
+                        {comments.map((comment) => {
+                            return(
+                                <Fade in key={comment.id}>
+                                    <li>
+                                        <p>{comment.comment}</p>
+                                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    </li>
+                                </Fade>
+                            );
+                        })}
+                    </Stagger>
+                </ul>
+                <CommentForm dishId={dishId} postComment={postComment} />
+            </div>
+        );
     }
+    else {
+        return (
+            <div></div>
+        );
+    }
+}
 
 
-    const DishDetail = (props) => {
-        if (props.isLoading) {
-            return(
-                <div className="container">
-                    <div className="row">
-                        <Loading />
-                    </div>
+const DishDetail = (props) => {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
-            );
-        }
-        else if (props.errMess) {
-            return(
-                <div className="container">
-                    <div className="row">
-                        <h4>{props.errMess}</h4>
-                    </div>
-                </div>
-            );
-        }
-        else if (props.dish != null) {
-            return(
-                <div className="container">
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{props.dish.name}</h3>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <RenderDish dish={props.dish} />
-                        <RenderComments comments={props.comments}
-                            postComment={props.postComment}
-                            dishId={props.dish.id} />
-                    </div>
-                </div>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
+            </div>
+        );
     }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish != null) {
+        return(
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments}
+                        postComment={props.postComment}
+                        dishId={props.dish.id} />
+                </div>
+            </div>
+        );
+    }
+    else {
+        return(
+            <div></div>
+        );
+    }
+}
 
 export default DishDetail;
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 
+
 export const Dishes = (state = {
         isLoading: true,
         errMess: null,
